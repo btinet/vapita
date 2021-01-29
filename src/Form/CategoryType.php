48590ddc97574a107722d34bbca12ec8c4fdca66
Benjamin Voigt <benjamin.voigt@vapita.de>
2021-01-29 10:19:49 +0100

    template updated

--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -31,7 +31,7 @@ class CategoryType extends AbstractType
             ])
             ->add('redirectToPost')
             ->add('isDarkLocalMenu')
-            ->add('isLead')
+            ->add('isLead', BooleanType::class)
             ->add('isShown')
             ->add('description')
             ->add('meteDescription')
