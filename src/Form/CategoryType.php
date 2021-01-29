ca0f2e0c794eaa0183b632290da4c49ee8024f14
Benjamin Voigt <benjamin.voigt@vapita.de>
2021-01-29 07:39:01 +0100

    Entity updated

--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -30,6 +30,7 @@ class CategoryType extends AbstractType
             ->add('redirectToPost')
             ->add('isDarkLocalMenu')
             ->add('isShown')
+            ->add('isLead')
             ->add('description')
             ->add('meteDescription')
             ->add('parent', EntityType::class, [
